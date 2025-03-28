7e3721e11fb52fc9a594c05e06b150953f80b697
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-03-28 12:21:02 +0100

    vyřešeno

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//Použijte funkci prompt a zeptejte se uživatele na správnou odpověď. 
+// Vyberte element s třídou answer__text a nastavte obsah elementu na odpověď od uživatele. 
+// Vyberte element s třidou answer. Pokud je odpověď příjmení Bell, přidejte k tomuto elementu třídu answer--correct. 
+// V opačném případě přidejte třídu answer--wrong.
 
+const question = document.querySelector('.question')
+question.textContent= "⚡️Jak se jmenoval Ten Jehož Jméno Nesmíme Vyslovit?⚡️"
+
+const personalAnswer = prompt('Zadejte odpověď:').toLowerCase
+const answer__text = personalAnswer
+const answer = document.querySelector('.answer')
+
+if (personalAnswer === 'Lord Voldemort'.toLowerCase) {
+    answer.classList.add('answer--correct')
+} else answer.classList.add('answer--wrong')
